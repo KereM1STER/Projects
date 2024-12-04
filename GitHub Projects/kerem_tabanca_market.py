2e417e4272a64b5d78a3162e312c79de2f247da3
KereM1STER <110399809+KereM1STER@users.noreply.github.com>
2024-12-04 17:48:49 +0300

    Update kerem_tabanca_market.py
    asd

--- GitHub Projects/kerem_tabanca_market.py
+++ GitHub Projects/kerem_tabanca_market.py
@@ -15,7 +15,7 @@ print("**** İstinye Online Market’e Hoşgeldiniz ****\n")
 print("***********************************************")
 #hjasbdjashdbajdhajhd1123123123131231231231
 #DENAKSDBKJABDKJABSDBHAKDJKA
-#Beğendim Lİke
+#Beğendim Lİkeasdasdasd
 #kullanıcıdan bilgilerini girmesini(pdf te verildiği gibi) sağladık.
 def giris_ekranı():
     
