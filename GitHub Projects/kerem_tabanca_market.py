84c924c5010e9534ca71cfc9828a66a2f68535b0
KereM1STER <110399809+KereM1STER@users.noreply.github.com>
2024-12-05 11:50:50 +0300

    Update kerem_tabanca_market.py

--- GitHub Projects/kerem_tabanca_market.py
+++ GitHub Projects/kerem_tabanca_market.py
@@ -1,6 +1,6 @@
 import datetime,time
 # kütüphaneleri çağırdık time.sleep ve datetime komutları için.
-nick = ""
+nick = "kerem"
 Envanter = {"kuşkonmaz": [10,5],"brokoli": [15,6],"havuç": [18,7],"elmalar": [20,5],"muz":
 [10,8],"meyve": [30,3],"yumurta": [50,2],"karışık meyve suyu": [0,18],"balık çubukları":
 [25,12],"dondurma": [32,6],"elma suyu": [40,7],"portakal suyu": [30,8],"üzüm suyu": [10,9]}
