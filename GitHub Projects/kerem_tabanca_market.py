ee54a664f2ef817ce52678687a756bffc875fc75
KereM1STER <110399809+KereM1STER@users.noreply.github.com>
2024-12-05 11:47:25 +0300

    Update kerem_tabanca_market.py

--- GitHub Projects/kerem_tabanca_market.py
+++ GitHub Projects/kerem_tabanca_market.py
@@ -14,7 +14,7 @@ print("***********************************************")
 print("**** İstinye Online Market’e Hoşgeldiniz ****\n")
 print("***********************************************")
 #hjasbdjashdbajdhajhd1123123123131231231231
-#shahdahsdhas
+#shahdahsdhas11111
 #Beğendim Lİkeasdasdasd asdadasdasdad
 
 #Beğendim Lİke
